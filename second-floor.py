7ba4db514b41f5272f6f368139d02f111f4f471e
Oleg Levitsky <levabd@gmail.com>
2021-02-02 12:06:23 +0200

    add the second floor logic

--- second-floor.py
+++ second-floor.py
@@ -2,26 +2,29 @@
 
 """Demo file showing how to use the mitemp library."""
 
-import os
-import subprocess
-import paramiko
 import json
 import argparse
 import re
 import datetime
+import paramiko
 import requests
 
-# cmd = ['ssh', 'smart',
-#        'mkdir -p /home/levabd/smart-home-temp-humidity-monitor; cat - > /home/levabd/smart-home-temp-humidity-monitor/lr.json']
+# cmd  ['ssh', 'smart',
+#        'mkdir -p /home/levabd/smart-home-temp-humidity-monitor;
+#         cat - > /home/levabd/smart-home-temp-humidity-monitor/lr.json']
 
-from miio import chuangmi_plug
-from btlewrap import available_backends, BluepyBackend, GatttoolBackend, PygattBackend
+from btlewrap import available_backends, BluepyBackend
 from mitemp_bt.mitemp_bt_poller import MiTempBtPoller, \
     MI_TEMPERATURE, MI_HUMIDITY, MI_BATTERY
 
-state = {}
+br_state = {}
+cb_state = {}
 f = open('/home/pi/smart-climat-daemon/ac_br_state.json')
-state = json.load(f)
+br_state = json.load(f)
+f = open('/home/pi/smart-climat-daemon/ac_cb_state.json')
+cb_state = json.load(f)
+
+dummy_ac_url = 'http://smart.levabd.pp.ua:2002'
 
 def valid_mitemp_mac(mac, pat=re.compile(r"[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}")):
     """Check for valid mac addresses."""
@@ -30,91 +33,151 @@ def valid_mitemp_mac(mac, pat=re.compile(r"[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[
             'The MAC address "{}" seems to be in the wrong format'.format(mac))
     return mac
 
-# WiFi Router Plug token anf IP miplug --token e171f0529f5c458744756664b75acfa1 --ip 192.168.19.61 on
-# def turn_on_humidifier():
-#     """Turn on humidifier on a second floor."""
-#     cP = chuangmi_plug.ChuangmiPlug(ip='192.168.19.61', token='eca25f7d91a6034a978af9900ff2d3f2', start_id=0, debug=1, lazy_discover=True, model='chuangmi.plug.m1')
-#     cP.on()
-
-
+#  turn_on_humidifier():
+#     """Turn on humidifier on a first floor."""
+#     hummidifier_plug = chuangmi_plug.ChuangmiPlug(
+#         ip='192.168.19.61',
+#         token='14f5b868a58ef4ffaef6fece61c65b16',
+#         start_id=0,
+#         debug=1,
+#         lazy_discover=True,
+#         model='chuangmi.plug.m1')
+#     hummidifier_plug.on()
+#
+#
 # def turn_off_humidifier():
-#     """Turn off humidifier on a second floor."""
-#     cP = chuangmi_plug.ChuangmiPlug(ip='192.168.19.61', token='eca25f7d91a6034a978af9900ff2d3f2', start_id=0, debug=1, lazy_discover=True, model='chuangmi.plug.m1')
-#     cP.off()
+#     """Turn off humidifier on a first floor."""
+#     hummidifier_plug = chuangmi_plug.ChuangmiPlug(
+#         ip='192.168.19.61',
+#         token='14f5b868a58ef4ffaef6fece61c65b16',
+#         start_id=0,
+#         debug=1,
+#         lazy_discover=True,
+#         model='chuangmi.plug.m1')
+#     hummidifier_plug.off()
 
-
-def check_if_ac_off():
+def check_if_ac_off(room):
     """Check if AC is turned off."""
-    status_url = 'http://smart.levabd.pp.ua:2002/status-bedroom?key=27fbc501b51b47663e77c46816a'
+    status_url = dummy_ac_url
+    if room == 'br':
+        status_url = 'http://smart.levabd.pp.ua:2002/status-bedroom?key=27fbc501b51b47663e77c46816a'
+    elif room == 'cb':
+        status_url = 'http://smart.levabd.pp.ua:2002/status-office?key=27fbc501b51b47663e77c46816a'
     response = requests.get(status_url, timeout=(20, 30))
-    if ('address' in response.json()) and ('name' in response.json()):
-        if ((response.json()['name'] == "08bc20043df8") and (response.json()['address'] == "192.168.19.54")):
-            if response.json()['props']['boot'] == 0:
-                return True
+    if 'Pow' in response.json():
+        print(response.json()['Pow'])
+        if response.json()['Pow'] == "1":
             return False
+        return True
     return None
 
-def check_if_ac_cool():
+def check_if_ac_cool(room):
     """Check if AC is turned for a automate cooling."""
-    status_url = 'http://smart.levabd.pp.ua:2003/status/key/27fbc501b51b47663e77c46816a'
+    status_url = dummy_ac_url
+    if room == 'br':
+        status_url = 'http://smart.levabd.pp.ua:2002/status-bedroom?key=27fbc501b51b47663e77c46816a'
+    elif room == 'cb':
+        status_url = 'http://smart.levabd.pp.ua:2002/status-office?key=27fbc501b51b47663e77c46816a'
     response = requests.get(status_url, timeout=(20, 30))
-    if ('address' in response.json()) and ('name' in response.json()):
-        if ((response.json()['name'] == "08bc20043df8") and (response.json()['address'] == "192.168.19.54")):
-            if not response.json()['props']['boot'] == 1:
-                return False
-            if not response.json()['props']['runMode'] == '001':
-                return False
-            if not response.json()['props']['wdNumber'] == 25:
-                return False
-            if not response.json()['props']['windLevel'] == '001':
-                return False
+    if 'Pow' in response.json():
+        print(response.json()['Pow'])
+        if (response.json()['Pow'] == "1") and (response.json()['Mode'] == "COOL"):
             return True
+        return False
     return None
 
-def turn_on_cool_ac():
+def set_cool_temp_ac(room, temp):
+    """Set AC temerature of cooling if AC already turned cool."""
+    state = {}
+    state = br_state if room == 'br' else cb_state # 'cb'
+    if not state['wasTurnedCool'] == 1 and check_if_ac_cool(room):
+        return
+    temp_url = dummy_ac_url
+    if room == 'br':
+        temp_url = 'http://smart.levabd.pp.ua:2002/setTemp-bedroom?key=27fbc501b51b47663e77c46816a&temp='
+    elif room == 'cb':
+        temp_url = 'http://smart.levabd.pp.ua:2002/setTemp-office?key=27fbc501b51b47663e77c46816a&temp='
+
+    response = requests.get(temp_url + temp)
+    print(response.json())
+
+
+def turn_on_cool_ac(room):
     """Turn on AC for a cooling if it was not."""
-    if (state['wasTurnedCool'] == 1) and not (state['triedTurnedCool'] == 1):
+    state = {}
+    state = br_state if room == 'br' else cb_state # 'cb'
+    ac_cool = check_if_ac_cool(room)
+    if ((state['wasTurnedCool'] == 1) and not state['triedTurnedCool'] == 1) or (ac_cool is None):
         return
-    cool_url = 'http://smart.levabd.pp.ua:2003/cool/key/27fbc501b51b47663e77c46816a'
-    ac_cool = check_if_ac_cool()
-    if ac_cool is not None:
-        if not ac_cool:
-            state['triedTurnedCool'] = 1
-            state['wasTurnedCool'] = 0
-            response = requests.get(cool_url)
-            print(response.json())
-        else:
-            if (state['triedTurnedCool'] == 1):
-                state['triedTurnedOff'] = 0
-                state['wasTurnedOff'] = 0
-                state['triedTurnedCool'] = 0
-                state['wasTurnedCool'] = 1
-                state['triedTurnedHeat'] = 0
-                state['wasTurnedHeat'] = 0
+    if ac_cool and (state['triedTurnedCool'] == 1):
+        if room == 'br':
+            br_state['triedTurnedOff'] = 0
+            br_state['wasTurnedOff'] = 0
+            br_state['triedTurnedCool'] = 0
+            br_state['wasTurnedCool'] = 1
+            br_state['triedTurnedHeat'] = 0
+            br_state['wasTurnedHeat'] = 0
+        elif room == 'cb':
+            cb_state['triedTurnedOff'] = 0
+            cb_state['wasTurnedOff'] = 0
+            cb_state['triedTurnedCool'] = 0
+            cb_state['wasTurnedCool'] = 1
+            cb_state['triedTurnedHeat'] = 0
+            cb_state['wasTurnedHeat'] = 0
+        return
+    cool_url = dummy_ac_url
+    if room == 'br':
+        cool_url = 'http://smart.levabd.pp.ua:2002/cool-bedroom?autoFan=false&key=27fbc501b51b47663e77c46816a'
+    elif room == 'cb':
+        cool_url = 'http://smart.levabd.pp.ua:2002/cool-office?autoFan=false&key=27fbc501b51b47663e77c46816a'
+    if room == 'br':
+        br_state['triedTurnedCool'] = 1
+        br_state['wasTurnedCool'] = 0
+    elif room == 'cb':
+        cb_state['triedTurnedCool'] = 1
+        cb_state['wasTurnedCool'] = 0
+    response = requests.get(cool_url)
+    print(response.json())
 
 
-def turn_off_ac():
+def turn_off_ac(room):
     """Turn off AC ."""
-    if (state['wasTurnedOff'] == 1) and not (state['triedTurnedOff'] == 1):
+    state = {}
+    state = br_state if room == 'br' else cb_state # 'cb'
+    ac_off = check_if_ac_off(room)
+    if ((state['wasTurnedOff'] == 1) and not state['triedTurnedOff'] == 1) or (ac_off is None):
         return
-    turn_url = 'http://smart.levabd.pp.ua:2003/power-off/key/27fbc501b51b47663e77c46816a'
-    ac_off = check_if_ac_off()
-    if ac_off is not None:
-        if not ac_off:
-            state['triedTurnedOff'] = 1
-            state['wasTurnedOff'] = 0
-            response = requests.get(turn_url)
-            print(response.json())
-        else:
-            if state['triedTurnedOff'] == 1:
-                state['triedTurnedOff'] = 0
-                state['wasTurnedOff'] = 1
-                state['triedTurnedCool'] = 0
-                state['wasTurnedCool'] = 0
-                state['triedTurnedHeat'] = 0
-                state['wasTurnedHeat'] = 0
+    if ac_off and (state['triedTurnedCool'] == 1):
+        if room == 'br':
+            br_state['triedTurnedOff'] = 0
+            br_state['wasTurnedOff'] = 1
+            br_state['triedTurnedCool'] = 0
+            br_state['wasTurnedCool'] = 0
+            br_state['triedTurnedHeat'] = 0
+            br_state['wasTurnedHeat'] = 0
+        elif room == 'cb':
+            cb_state['triedTurnedOff'] = 0
+            cb_state['wasTurnedOff'] = 1
+            cb_state['triedTurnedCool'] = 0
+            cb_state['wasTurnedCool'] = 0
+            cb_state['triedTurnedHeat'] = 0
+            cb_state['wasTurnedHeat'] = 0
+    turn_url = dummy_ac_url
+    if room == 'br':
+        turn_url = 'http://smart.levabd.pp.ua:2002/powerOff-bedroom?key=27fbc501b51b47663e77c46816a'
+    elif room == 'cb':
+        turn_url = 'http://smart.levabd.pp.ua:2002/powerOff-office?autoFan=false&key=27fbc501b51b47663e77c46816a'
+    if room == 'br':
+        br_state['triedTurnedOff'] = 1
+        br_state['wasTurnedOff'] = 0
+    elif room == 'cb':
+        cb_state['triedTurnedOff'] = 1
+        cb_state['wasTurnedOff'] = 0
+    response = requests.get(turn_url)
+    print(response.json())
 
-def recordTempHumid(temperature, humidity, room):
+def record_temp_humid(temperature, humidity, room):
+    """Record temperature and humidity data for web interface monitor"""
     dicty = {
         "temperature": temperature,
         "humidity": humidity
@@ -148,7 +211,7 @@ def poll_temp_humidity(room):
     print("Humidity: {}".format(poller.parameter_value(MI_HUMIDITY)))
     return (today, temperature, humidity)
 
-# def scan(args):
+#  scan(args):
 #     """Scan for sensors."""
 #     backend = _get_backend(args)
 #     print('Scanning for 10 seconds...')
@@ -166,9 +229,14 @@ def list_backends(_):
 
 
 def main():
-    """Main function.
+    """Main function."""
+    print(check_if_ac_off('br'))
+    print(check_if_ac_off('cb'))
 
-    """
+    # record the office room numbers
+    (_, temperature, humidity) = poll_temp_humidity('cb')
+    record_temp_humid(temperature, humidity, 'cb')
+
     # check bedroom
     (today, temperature, humidity) = poll_temp_humidity('br')
     # if (humidity > 49) and (today.month < 10) and (today.month > 4):
@@ -179,41 +247,52 @@ def main():
     #     turn_on_humidifier()
     # if (humidity > 49) and ((today.month > 9) or (today.month < 5)):
     #     turn_off_humidifier()
-    
+    #
     # Prevent Sleep of Xiaomi Smart Plug
-    # cP = chuangmi_plug.ChuangmiPlug(ip='192.168.19.61', token='14f5b868a58ef4ffaef6fece61c65b16', start_id=0, debug=0, lazy_discover=True, model='chuangmi.plug.m1')
-    # print(cP.status())
+    # hummidifier_plug = chuangmi_plug.ChuangmiPlug(
+    #   ip='192.168.19.59',
+    #     token='14f5b868a58ef4ffaef6fece61c65b16',
+    #     start_id=0,
+    #     debug=0,
+    #     lazy_discover=True,
+    #     model='chuangmi.plug.m1')
+    # print(hummidifier_plug.status())
 
     # Record temperature and humidity for monitor
-    recordTempHumid(temperature, humidity, 'br')
+    record_temp_humid(temperature, humidity, 'br')
 
     # clear env at night
     if today.hour == 3:
-        state['triedTurnedOff'] = 0
-        state['wasTurnedOff'] = 0
-        state['triedTurnedCool'] = 0
-        state['wasTurnedCool'] = 0
-        state['triedTurnedHeat'] = 0
-        state['wasTurnedHeat'] = 0
+        br_state['triedTurnedOff'] = 0
+        br_state['wasTurnedOff'] = 0
+        br_state['triedTurnedCool'] = 0
+        br_state['wasTurnedCool'] = 0
+        br_state['triedTurnedHeat'] = 0
+        br_state['wasTurnedHeat'] = 0
+        cb_state['triedTurnedOff'] = 0
+        cb_state['wasTurnedOff'] = 0
+        cb_state['triedTurnedCool'] = 0
+        cb_state['wasTurnedCool'] = 0
+        cb_state['triedTurnedHeat'] = 0
+        cb_state['wasTurnedHeat'] = 0
 
-    with open('/home/pi/smart-climat-daemon/ac_br_state.json', 'w') as f:
-        json.dump(state, f)
+    with open('/home/pi/smart-climat-daemon/ac_br_state.json', 'w') as file:
+        json.dump(br_state, file)
+    with open('/home/pi/smart-climat-daemon/ac_cb_state.json', 'w') as file:
+        json.dump(cb_state, file)
 
     if (temperature > 24.0) and (today.month < 6) and (today.month > 3) and (today.hour < 11) and (today.hour > 3):
-        turn_on_cool_ac()
+        turn_on_cool_ac('br')
     if (temperature > 27) and (today.hour < 24) and (today.hour > 10):
-        turn_on_cool_ac()
+        turn_on_cool_ac('br')
     if (temperature > 24.9) and (today.month < 10) and (today.month > 5) and (today.hour < 11) and (today.hour > 3):
-        turn_on_cool_ac()
-    if (temperature < 22.5):
-        turn_off_ac()
-    # if (temperature < 20) and ((today.month > 9) or (today.month < 5)) and (today.hour < 24) and (today.hour > 9):
+        turn_on_cool_ac('br')
+    if temperature < 22.5:
+        turn_off_ac('br')
+    # _if (temperature < 20) and ((today.month > 9) or (today.month < 5)) and (today.hour < 24) and (today.hour > 9):
     #     turn_on_heat_ac()
     # if (temperature > 22) and ((today.month > 9) or (today.month < 5)):
     #     turn_off_ac()
-
-    (today, temperature, humidity) = poll_temp_humidity('cb')
-    recordTempHumid(temperature, humidity, 'cb')
 
 
 if __name__ == '__main__':
